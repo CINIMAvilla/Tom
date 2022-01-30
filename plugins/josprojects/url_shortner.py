da98dc2427cefd8689c2e9f0bd40cc0ccbe0558a
Ammff <90828262+Ammff@users.noreply.github.com>
2022-01-30 12:08:31 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/josprojects')
+        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/CV_Community')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
+        shorten_urls += "\n\nmade by @CV_Community | @CV_Community 🔥"
         return shorten_urls
     except Exception as error:
         return error
