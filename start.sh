b9e19fc12673384eaea283f7b757740e5f078025
Ammff <90828262+Ammff@users.noreply.github.com>
2022-01-30 19:01:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Ammff/tgmoviebot.git /tgmoviebot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /tgmoviebot
-fi
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/Ammff/tgmoviebot.git /tgmoviebot
 cd /tgmoviebot
+echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot, Please Wait..."
 python3 bot.py
